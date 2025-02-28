d588ee54c48dcde5a975bd68940b1e5653b65ca5
shritija sawant <sawantshritija15@gmail.com>
2025-02-28 11:31:26 +0530

    Updated signup and login pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'authentication.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,41 +24,15 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Firebase Auth',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: TextButton(
-            onPressed: () async {
-              await FirebaseFirestore.instance
-                  .collection("Hackathon")
-                  .doc()
-                  .set({"name": "shitija"});
-            },
-            child: Text("data")),
-      ),
+      home: SignUpPage(),
     );
   }
 }
